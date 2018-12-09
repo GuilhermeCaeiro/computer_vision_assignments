e702a5584c1bd9f5822479f2eecbc383dff41c00
NOTEGUILHERMEJP\Guilherme <guilhermecaeirodemattos@gmail.com>
2018-12-08 23:43:05 -0200

    Best result until now, but the image is still distorted.

--- assignment2/method2.py
+++ assignment2/method2.py
@@ -64,30 +64,105 @@ class ApplicationUI:
 
 
         deformed_points = np.matrix([
-            [493, 368, 1],
             [6, 35, 1],
+            [493, 368, 1],
             [41, 155, 1],
             [291, 24, 1],
+
             
-            [493, 368, 1],
+            
             [6, 35, 1],
+            [493, 368, 1],
             [29, 375, 1],
             [460, 54, 1],
+            
+
             
             [107, 28, 1],
             [495, 246, 1],
             [41, 155, 1],
             [291, 24, 1],
+            
+
 
             [107, 28, 1],
             [495, 246, 1],
             [264, 352, 1],
             [471, 154, 1],
             
+
+            
             [192, 75, 1],
             [164, 274, 1],
             [92, 211, 1],
             [418, 204, 1],
+        ])
+
+        deformed_points = np.matrix([
+            [22, 159, 1],
+            [427, 202, 1],
+            [153, 168, 1],
+            [151, 473, 1],
+
+            
+            
+            [414, 266, 1],
+            [703, 290, 1],
+            [536, 223, 1],
+            [535, 618, 1],
+            
+
+            
+            [383, 746, 1],
+            [857, 656, 1],
+            [798, 411, 1],
+            [795, 715, 1],
+            
+
+
+            [765, 456, 1],
+            [1007, 447, 1],
+            [988, 388, 1],
+            [985, 707, 1],
+            
+
+            
+            #[87, 721, 1],
+            #[405, 673, 1],
+            #[229, 553, 1],
+            #[229, 769, 1],
+
+            [702, 6, 1],
+            [1039, 96, 1],
+            [800, 10, 1],
+            [795, 758, 1],
+        ])
+
+        deformed_points = np.matrix([
+            [154.37, 451.36, 1.0],
+            [152.575, 21.4933, 1.0],
+            [64.62, 320.609, 1.0],
+            [403.875, 336.729, 1.0],
+            
+            [256.685, 37.6133, 1.0],
+            [755.695, 128.96, 1.0],
+            [534.91, 91.3467, 1.0],
+            [534.91, 340.311, 1.0],
+            
+            [281.815, 617.933, 1.0],
+            [723.385, 567.782, 1.0],
+            [696.46, 347.476, 1.0],
+            [696.46, 515.84, 1.0],
+
+            [709.025, 343.893, 1.0],
+            [705.435, 120.00, 1.0],
+            [538.5, 211.351, 1.0],
+            [707.23, 232.844, 1.0],
+            
+            [337.46, 229.262, 1.0],
+            [538.5, 248.964, 1.0],
+            [337.46, 327.773, 1.0],
+            [335.665, 118.213, 1.0],
         ])
 
 
@@ -122,18 +197,26 @@ class ApplicationUI:
                 ((l[0, 0] * m[0, 2]) + (l[0, 2] * m[0, 0])) / 2,
                 ((l[0, 1] * m[0, 2]) + (l[0, 2] * m[0, 1])) / 2,
                 #(l[0, 2] * m[0, 2]),
+                #1.0
             ])
 
-            b.append( - (l[0, 2] * m[0, 2]))
+            b.append(  (l[0, 2] * m[0, 2]))
 
-        #system_of_equations.append([0, 0, 0, 0, 0, 1]) # last number is 1 to force f = 1
+        #system_of_equations.append([0, 0, 0, 0, 0, 0]) # last number is 1 to force f = 1
+        #b.append(0)
 
         print("System of equations: \n", system_of_equations)
+        print("b: ", b)
+        system_of_equations = np.matrix(system_of_equations)
 
         #b = np.matrix([[0, 0, 0, 0, 0, 1]]) # last number is 1 to force f = 1
         b = np.matrix([b])
 
-        x = np.linalg.inv(system_of_equations) * b.T
+        print(system_of_equations.shape, b.shape)
+
+        #x = np.linalg.inv(system_of_equations) * b.T
+        #x = np.linalg.solve(system_of_equations, b.T)
+        x = np.linalg.lstsq(system_of_equations, b.T)[0]
 
         print("x: \n", x)
 
@@ -144,6 +227,11 @@ class ApplicationUI:
         ])
 
         print("Conic matrix: \n", conic)
+
+        w, v = np.linalg.eig(conic)
+
+        print("Eigenvalues: \n", w)
+        print("Eigenvectors: \n", v)
 
         """
 
@@ -167,6 +255,7 @@ class ApplicationUI:
         print("v matrix: \n", v)
 
         K = scipy.linalg.cholesky(KK_T, lower = False)
+        #K = np.linalg.qr(KK_T, "r")
 
         print("K matrix: \n", K)
 
@@ -189,24 +278,31 @@ class ApplicationUI:
 
 
         # matrix to transform from projective space to similarity ()
-        projective_to_similarity_homography_matrix = projective_homography * affine_homography  
+        projective_to_similarity_homography_matrix = projective_homography * affine_homography
         #"""
 
         #"""
         # Based on https://github.com/jimmysoda/imgrectifier/blob/master/proj2metric_circle.m
 
-        w, v = np.linalg.eig(conic)
+        #w, v = np.linalg.eig(conic)
 
-        print("Eigenvalues: \n", w)
-        print("Eigenvectors: \n", v)
+        #print("Eigenvalues: \n", w)
+        #print("Eigenvectors: \n", v)
 
         v_transp = v.T
+        #w[0] = w[0] * -1 if w[0] < 0 else w[0]
+        #w[1] = w[1] * -1 if w[1] < 0 else w[1]
 
-        ss = np.matrix([
-            [np.sqrt(w[0]), 0, 0],
-            [0, np.sqrt(w[1]), 0],
-            [0, 0, 10]
-        ])
+        print(w)
+
+        #ss = np.matrix([
+        #    [np.sqrt(w[0]), 0, 0],
+        #    [0, np.sqrt(w[1]), 0],
+        #    [0, 0, 10]
+        #])
+
+        ss = np.diag(w)
+        print(ss)
 
         v_transp = ss * v_transp
         v = v_transp
@@ -216,7 +312,62 @@ class ApplicationUI:
         projective_to_similarity_homography_matrix = v
         #"""
 
+        """
 
+        KK_T = np.matrix([
+            [x[0, 0], x[1, 0] / 2],
+            [x[1, 0] / 2, x[2, 0]],
+        ])
+
+        print("KK.T matrix: \n", KK_T)
+
+        KK_T_v = np.matrix([
+            [x[3, 0] / 2],
+            [x[4, 0] / 2],
+        ])
+
+        print("KK.T * v matrix: \n", KK_T_v)
+
+        v = np.linalg.inv(KK_T) * KK_T_v
+        #v = np.linalg.lstsq(KK_T, KK_T_v)[0]
+
+        print("v matrix: \n", v)
+
+        U, S, V = np.linalg.svd(KK_T)
+        
+        print("U", U)
+        print("S", S)
+        #S = np.asmatrix(S).T
+        S = np.matrix([[S[0], 0], [0, S[1]]])
+        print("S", S)
+        print("V", V)
+
+        K = U * np.sqrt(S)
+        K = K * V ###
+
+
+        print(K)
+
+        projective_homography = np.matrix([
+            [1, 0, 0],
+            [0, 1, 0],
+            [v[0, 0], v[1, 0], 1],
+        ])
+
+        print("Projective homography: \n", projective_homography)
+
+        affine_homography = np.matrix([
+            [K[0, 0], K[0, 1], 0],
+            [K[1, 0], K[1, 1], 0],
+            [0, 0, 1],
+        ])
+
+        print("Affine homography: \n", affine_homography)
+
+        # matrix to transform from projective space to similarity ()
+        projective_to_similarity_homography_matrix = projective_homography * affine_homography
+
+        #"""
 
         # Draw lines. Only for debugging purposes, because the self.run will stop the execution of the remainder of this method.
         #self.image_canvas.create_line(deformed_points[0, 0], deformed_points[0, 1], deformed_points[1, 0], deformed_points[1, 1], width=5, fill='green')
